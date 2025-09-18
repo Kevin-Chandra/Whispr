e4abc1b0d6f4ac5c1a6bade08109d076a2c0369c
Kevin Chandra <kevin.chandra@snappymob.com>
2025-09-18 13:59:28 +0800

    Update theme and add gradient text

--- lib/presentation/themes/colors.dart
+++ lib/presentation/themes/colors.dart
@@ -3,9 +3,17 @@ import 'dart:ui';
 abstract class WhisprColors {
   static const mauve = Color(0xFFE5ACF3);
   static const lavenderBlue = Color(0xFFC9C4FF);
+  static const vodka = Color(0xFFBCB7FC);
   static const magnolia = Color(0xFFFBE8FF);
   static const paleMagnolia = Color(0xFFFCF1FF);
   static const lavenderWeb = Color(0xFFDEE5FF);
   static const cornflowerBlue = Color(0xFF6A86E9);
   static const spanishViolet = Color(0xFF452B88);
+
+  // Negative colors.
+  static const crayola = Color(0xFFFF5449);
+  static const melon = Color(0xFFfab1ac);
+
+  // Neutral colors.
+  static const lightGray = Color(0xFFD5D5D5);
 }
