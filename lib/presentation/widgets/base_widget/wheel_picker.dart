80774b68fd82c6f939d48955c54c5f5e8e49c50f
Kevin Chandra <kevin.chandra@snappymob.com>
2025-09-20 18:06:34 +0800

    Fix wheel_picker to fit max width/height according to scroll axis

---
+++ lib/presentation/widgets/base_widget/wheel_picker.dart
@@ -0,0 +1,348 @@
+import 'dart:math' as math;
+
+import 'package:flutter/material.dart';
+
+import './wheel_picker_style.dart';
+
+part './wheel_picker_controller.dart';
+
+/// A customizable scrolling wheel interface for selecting items, such as numbers, dates, or any value of your choice.
+///
+/// Key Features:
+/// - Supports both looping and non-looping scrolling behavior.
+/// - Customizable appearance and styling through `WheelPickerStyle`.
+/// - Enables tap gestures for intuitive navigation through the wheel.
+/// - Easily integrates with a controller for precise control and synchronization.
+/// - Provides the ability to highlight the selected item with a color shader.
+///
+/// For more control, use it with the `WheelPickerController`.
+///
+/// > _Note: If not wrapped or constrained, the widget will take up all available space._
+///
+/// Usage:
+/// `WheelPicker` is highly configurable and can be adapted to different use cases by specifying the builder function, initial settings, and style properties. It's a valuable component for creating user-friendly and visually appealing selection interfaces in your Flutter applications.
+///
+/// Example:
+/// ```dart
+/// WheelPicker(
+///   builder: (context, index) {
+///     // Define how each item in the wheel should be rendered.
+///     return Text('Item $index');
+///   },
+///   itemCount: 10, // Number of items in the wheel.
+///   initialIndex: 2, // Initial selected item.
+///   looping: true, // Enable looping behavior.
+///   selectedIndexColor: Colors.blue, // Color to highlight the selected item.
+///   enableTap: true, // Allow tap gestures for navigation.
+///   onIndexChanged: (index, interactionType) {
+///     // Handle index changes.
+///     print('Selected index: $index from interaction: ${interactionType.name}');
+///   },
+///   style: WheelPickerStyle(
+///     // Customize the appearance and behavior.
+///     itemExtent: 50,
+///     diameterRatio: .8
+///     // ...
+///   ),
+/// )
+/// ```
+///
+/// Simplify the process of creating scrollable selection wheels in Flutter, making it easier to build interactive and engaging user interfaces.
+class WheelPicker extends StatefulWidget {
+  /// Callback for creating item widgets based on their index.
+  final Widget Function(BuildContext context, int index) builder;
+
+  /// The total number of items. This is used when no controller is provided.
+  final int? itemCount;
+
+  /// A controller for retrieving the selected item index, controlling wheel behavior, shifting it, and enabling mounting of additional [WheelPickerController] controllers for shifting them when looping.
+  ///
+  /// If a controller is initialized, [itemCount] and [initialIndex] must not be specified.
+  final WheelPickerController? controller;
+
+  /// The initially selected item index. Use this property when not using a [controller].
+  final int? initialIndex;
+
+  /// Whether the wheel should support infinite looping, allowing it to loop both forward and backward.
+  ///
+  /// Must be `true` for automatic shifting for mounted controllers.
+  final bool looping;
+
+  /// Determines how the wheel is oriented and scrolled.
+  final Axis scrollDirection;
+
+  /// The color used to highlight the selected item(center) using a shader mask.
+  final Color? selectedIndexColor;
+
+  /// Whether to enable tap gestures for item selection.
+  final bool enableTap;
+
+  /// Callback function triggered when the selected item index changes.
+  ///
+  /// Provides the updated [index] and the [interactionType] that caused the change.
+  final void Function(int index, WheelPickerInteractionType interactionType)?
+      onIndexChanged;
+
+  /// Defines the appearance and behavior style for the `WheelPicker`.
+  final WheelPickerStyle style;
+
+  /// Creates a `WheelPicker` widget with customizable options.
+  ///
+  /// Use this constructor to create a scrollable selection wheel with specific settings. You can define the appearance, behavior, and provide callbacks for user interaction.
+  const WheelPicker({
+    super.key,
+    required this.builder,
+    this.itemCount,
+    this.controller,
+    this.initialIndex,
+    this.looping = true,
+    this.scrollDirection = Axis.vertical,
+    this.selectedIndexColor,
+    this.enableTap = true,
+    this.onIndexChanged,
+    this.style = const WheelPickerStyle(),
+  })  : assert(
+          !(itemCount == null && controller == null),
+          "Must have either itemCount or a controller",
+        ),
+        assert(
+          !(itemCount != null && controller != null),
+          "Can't have both itemCount and controller.",
+        ),
+        assert(
+          !(initialIndex != null && controller != null),
+          "Can't have both initialIndex and controller.",
+        );
+
+  @override
+  State<WheelPicker> createState() => _WheelPickerState();
+}
+
+class _WheelPickerState extends State<WheelPicker> {
+  /// Creates a [WheelPickerController] instance based on the specified controller,
+  /// or falls back to creating a new one if no controller is provided.
+  ///
+  /// If `_controller` falls back to being created, it implies that there is no
+  /// specified controller, and consequently, `itemCount` must not be null.
+  ///
+  /// Note that if a new controller is made, it also gets disposed within the widget's scope.
+  late final WheelPickerController _controller = widget.controller ??
+      WheelPickerController(
+        itemCount: widget.itemCount!,
+        initialIndex:
+            widget.initialIndex ?? WheelPickerController._defaultInitialIndex,
+      );
+
+  /// Retrieves the range (item count) from the already initialized [_controller].
+  int _getRange() => _controller.itemCount;
+
+  /// Attaches the controller to the widget, configuring looping and shift animation style.
+  @override
+  void initState() {
+    // Ensure itemCount is valid.
+    assert(_getRange() > 0, "itemCount can't be less or equal to zero.");
+
+    // Attach the controller and configures its behavior.
+    _controller._attach();
+    _controller._updateShiftAnimationStyle(widget.style.shiftAnimationStyle);
+
+    super.initState();
+  }
+
+  /// Update widget props for any possible changes.
+  ///
+  /// This is to update properties changes with Hot-Reload without needing to Hot-Restart.
+  @override
+  void didUpdateWidget(covariant WheelPicker oldWidget) {
+    _controller._updateShiftAnimationStyle(widget.style.shiftAnimationStyle);
+
+    // In the case the widget's itemCount property is given then update the internal controller's itemCount.
+    if (widget.itemCount != null && widget.itemCount != _controller.itemCount) {
+      _controller.itemCount = widget.itemCount!;
+      // Also update the internal controller's current to its initialIndex.
+      _controller.setCurrent(_controller.initialIndex);
+    }
+
+    super.didUpdateWidget(oldWidget);
+  }
+
+  /// Dispose of the [_controller] if it's not the specified controller but was instead created within the widget's scope.
+  @override
+  void dispose() {
+    if (_controller != widget.controller) {
+      _controller.dispose();
+    }
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final range = _getRange();
+
+    Widget wheel = widget.looping
+        ? _loopingWheel(context, range)
+        : _nonLoopingWheel(context, range);
+
+    wheel = _gestureDetectsWrapper(wheel: wheel, enableTap: widget.enableTap);
+
+    if (widget.selectedIndexColor != null) {
+      wheel = _centerColorShaderMaskWrapper(wheel: wheel);
+    }
+
+    return LayoutBuilder(builder: (context, constraint) {
+      return SizedBox(
+        width: widget.scrollDirection == Axis.vertical
+            ? widget.style.itemExtent
+            : constraint.maxHeight,
+        height: widget.scrollDirection == Axis.vertical
+            ? constraint.maxWidth
+            : widget.style.itemExtent,
+        child: RotatedBox(
+          quarterTurns: widget.scrollDirection == Axis.vertical ? 0 : -1,
+          child: wheel,
+        ),
+      );
+    });
+  }
+
+  /// Factory method that updates possible [widget.controller] and calls possible [widget.onIndexChanged]
+  /// with the relative based on `index` and given `range`.
+  void Function(int)? _onIndexChangedMethodFactory(int range) {
+    return (int index) {
+      _controller._update(index);
+      widget.onIndexChanged?.call(index % range, _controller._interactionType);
+    };
+  }
+
+  /// Constructs a looping scroll wheel with items that are passed to a delegate with lazily built during layout.
+  Widget _loopingWheel(BuildContext context, int range) {
+    return ListWheelScrollView.useDelegate(
+      itemExtent: widget.style.itemExtent,
+      childDelegate: ListWheelChildBuilderDelegate(
+        builder: (context, index) => _scrollDirectionWrapper(
+          rotateClockwise: true,
+          child: widget.builder(context, index % range),
+        ),
+      ),
+      controller: _controller._scrollController,
+      onSelectedItemChanged: _onIndexChangedMethodFactory(range),
+      physics: const FixedExtentScrollPhysics(),
+      diameterRatio: widget.style.diameterRatio,
+      squeeze: widget.style.squeeze,
+      overAndUnderCenterOpacity: widget.style.surroundingOpacity,
+      magnification: widget.style.magnification,
+    );
+  }
+
+  /// Constructs a non looping scroll wheel with items that are passed to a delegate with lazily built during layout.
+  Widget _nonLoopingWheel(BuildContext context, int range) {
+    return ListWheelScrollView(
+      itemExtent: widget.style.itemExtent,
+      controller: _controller._scrollController,
+      onSelectedItemChanged: _onIndexChangedMethodFactory(range),
+      physics: const FixedExtentScrollPhysics(),
+      diameterRatio: widget.style.diameterRatio,
+      squeeze: widget.style.squeeze,
+      overAndUnderCenterOpacity: widget.style.surroundingOpacity,
+      magnification: widget.style.magnification,
+      children: List.generate(range, (index) {
+        return _scrollDirectionWrapper(
+          rotateClockwise: true,
+          child: widget.builder(context, index),
+        );
+      }),
+    );
+  }
+
+  /// Rotates child if [widget.scrollDirection] is horizontal.
+  ///
+  /// Rotation is either +90 or -90 degrees based on [rotateClockwise].
+  /// (used to rotate the wheel and its items on different directions to achieve
+  /// horizontal scroll view).
+  Widget _scrollDirectionWrapper({
+    required bool rotateClockwise,
+    required Widget child,
+  }) {
+    if (widget.scrollDirection == Axis.vertical) return child;
+    return Transform.rotate(
+      angle: (math.pi * (rotateClockwise ? 1 : -1)) / 2,
+      child: child,
+    );
+  }
+
+  /// Wraps the [wheel] widget with a [GestureDetector] to detect tap and drag gestures.
+  ///
+  /// If [enableTap] is true, the method handles tap interactions to shift the wheel.
+  /// The tap location is calculated as a ratio from -1 to 1, with 0 representing the center of the wheel.
+  /// If the tap's offset exceeds the wheel's `itemExtent` ratio, the wheel will shift either up or down,
+  /// depending on the direction of the offset. A negative offset (above center) shifts the wheel up,
+  /// and a positive offset (below center) shifts it down. The interaction type is set to `WheelPickerInteractionType.tap`
+  /// when a tap is detected.
+  ///
+  /// If [enableTap] is false, tap interactions are disabled, and no shift occurs on tap.
+  ///
+  /// The drag interaction sets the interaction type to `drag`.
+  Widget _gestureDetectsWrapper(
+      {required Widget wheel, required bool enableTap}) {
+    // The layout builder is for getting the maxHeight which is the wheel size.
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        final wheelSize = constraints.maxHeight;
+        final offCenterRatio = (widget.style.itemExtent / wheelSize);
+        return GestureDetector(
+          onTapUp: enableTap
+              ? (details) {
+                  final tapLocation = details.localPosition.dy;
+                  final normalizedLocation =
+                      -1.0 + ((tapLocation / wheelSize) * 2.0);
+
+                  if (normalizedLocation.abs() >= offCenterRatio) {
+                    normalizedLocation > 0
+                        ? _controller._interactionShiftDown(
+                            WheelPickerInteractionType.tap)
+                        : _controller._interactionShiftUp(
+                            WheelPickerInteractionType.tap);
+                  }
+                }
+              : null,
+          onPanDown: (details) =>
+              _controller._setInteractionType(WheelPickerInteractionType.drag),
+          child: wheel,
+        );
+      },
+    );
+  }
+
+  /// Wrapps wheel with a [ShaderMask] of [BlendMode.srcATop] and places it on top
+  /// of the wheel's center with size of the `itemExtent` and `magnification`.
+  Widget _centerColorShaderMaskWrapper({required Widget? wheel}) {
+    return LayoutBuilder(
+      // The layout builder is for getting the maxHeight which is the wheel size.
+      builder: (context, constraints) {
+        final wheelSize = constraints.maxHeight;
+        final itemView = widget.style.itemExtent * widget.style.magnification;
+
+        // Shader stops.
+        final v = (1 - itemView / wheelSize) * .5;
+
+        return ShaderMask(
+          blendMode: BlendMode.srcATop,
+          shaderCallback: (Rect bounds) {
+            return LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              stops: [v, v, 1 - v, 1 - v],
+              colors: [
+                Colors.transparent,
+                widget.selectedIndexColor!,
+                widget.selectedIndexColor!,
+                Colors.transparent,
+              ],
+            ).createShader(bounds);
+          },
+          child: wheel,
+        );
+      },
+    );
+  }
+}
---
+++ lib/presentation/widgets/base_widget/wheel_picker_controller.dart
@@ -0,0 +1,280 @@
+part of './wheel_picker.dart';
+
+/// Represents the type of interaction with the WheelPicker that changes its index.
+///
+/// The available types are:
+/// - [drag]: The index changes when the wheel is dragged by the user.
+/// - [tap]: The index changes when the user taps on the wheel.
+/// - [control]: The index changes programmatically through `WheelPickerController` methods.
+/// - [mount]: The index changes due to external mounting when the WheelPicker loops.
+enum WheelPickerInteractionType { drag, tap, control, mount }
+
+/// Controller for managing and synchronizing `WheelPicker` widgets.
+///
+/// The `WheelPickerController` allows you to:
+/// - Retrieve the selected item index.
+/// - Shift the attached `WheelPicker` widget.
+/// - Set the current item selected.
+/// - Mount additional `WheelPickerController` instances, which will be shifted when looped.
+///
+/// It provides precise control over scrolling and synchronization of multiple `WheelPicker` widgets.
+///
+/// To use this controller:
+/// - Create an instance of `WheelPickerController`.
+/// - Connect it to one or more `WheelPicker` widgets using the `controller` property.
+/// - Use its methods to manipulate the selected item index and scrolling behavior.
+class WheelPickerController {
+  static const _defaultInitialIndex = 0;
+  static const _defaultInteractionType = WheelPickerInteractionType.drag;
+
+  /// The total number of items in the wheel - (can be changed).
+  int itemCount;
+
+  /// The initial selected item index.
+  final int initialIndex;
+
+  /// A list of `WheelPickerController` instances for shifting when looped.
+  final List<WheelPickerController> _mounts;
+
+  /// The scroll controller used for managing scroll behavior.
+  FixedExtentScrollController _scrollController;
+
+  /// The current selected item index.
+  int _current;
+
+  /// The previous cycle of the wheel (for looped scrolling).
+  int _previousCycle = 0;
+
+  /// Defines the type of interaction with the attached `WheelPicker` widget that changes the current index.
+  WheelPickerInteractionType _interactionType =
+      WheelPickerController._defaultInteractionType;
+
+  /// The animation style used for shifting the wheel.
+  WheelShiftAnimationStyle _shiftAnimationStyle =
+      const WheelPickerStyle().shiftAnimationStyle;
+
+  /// Creates a `WheelPickerController` with optional settings.
+  ///
+  /// Example usage:
+  /// ```dart
+  /// final controller = WheelPickerController(itemCount: 10, initialIndex: 2);
+  /// ```
+  ///
+  /// Note: Make sure to manually dispose of the controller when it is no longer needed to release associated resources.
+  WheelPickerController({
+    required this.itemCount,
+    this.initialIndex = WheelPickerController._defaultInitialIndex,
+    List<WheelPickerController>? mounts,
+  })  : _mounts = mounts ?? [],
+        _scrollController =
+            FixedExtentScrollController(initialItem: initialIndex),
+        _current = initialIndex;
+
+  /// Caps index to be ranging from 0 to `itemCount` - 1
+  int _capIndex(int index) => math.min(math.max(index, 0), itemCount - 1);
+
+  /// Wether scroll controller is attached to a mounted widget.
+  bool get _hasClients => _scrollController.hasClients;
+
+  /// Attaches the controller's [_scrollController] to a [WheelPicker] widget instance.
+  ///
+  /// If [_scrollController] `initialItem` differs from [_current], it updates
+  /// the scroll controller by replacing it with a new one and disposing of the previous one.
+  ///
+  /// * This method is intended for internal use by the [WheelPicker] widget.
+  void _attach() {
+    if (_scrollController.initialItem != _current) {
+      _scrollController.dispose();
+      _scrollController = FixedExtentScrollController(initialItem: _current);
+    }
+  }
+
+  /// Updates the shift animation style when the controller is attached during `initState`
+  /// or updated during `didUpdateWidget`.
+  ///
+  /// * This method is intended for internal use by the [WheelPicker] widget.
+  void _updateShiftAnimationStyle(WheelShiftAnimationStyle animationStyle) {
+    _shiftAnimationStyle = animationStyle;
+  }
+
+  /// Updates the controller with a new selected item index.
+  ///
+  /// Use this method to update the controller with the latest selected item based on the relative index from given index.
+  ///
+  /// Checks for optional cycle changes and calls [_shiftMounts] accordingly.
+  ///
+  /// * This method is intended for internal use by the [WheelPicker] widget.
+  void _update(int index) {
+    if (!_hasClients) return;
+    _current = index % itemCount;
+    final currentCycle = (index / itemCount).floor();
+    if (_previousCycle != currentCycle) {
+      final step = currentCycle > _previousCycle
+          ? VerticalDirection.down
+          : VerticalDirection.up;
+      _shiftMounts(step);
+      _previousCycle = currentCycle;
+    }
+  }
+
+  /// Shifts mounted controllers in the specified direction.
+  ///
+  /// Use this method to shift attached controllers based on the given `direction`.
+  ///
+  /// * This method is intended for internal use by the [WheelPicker] widget.
+  void _shiftMounts(VerticalDirection direction) {
+    const mountInteraction = WheelPickerInteractionType.mount;
+    final shiftMethod = direction == VerticalDirection.down
+        ? (WheelPickerController m) => m._interactionShiftDown(mountInteraction)
+        : (WheelPickerController m) => m._interactionShiftUp(mountInteraction);
+
+    for (final mount in _mounts) {
+      shiftMethod(mount);
+    }
+  }
+
+  /// Sets the interaction type.
+  /// This is used to record how the index change occurred (e.g., drag, tap, programmatic control).
+  /// * This method is intended for internal use by the [WheelPicker] widget.
+  void _setInteractionType(WheelPickerInteractionType interactionType) {
+    _interactionType = interactionType;
+  }
+
+  /// Shifts the wheel to the specified index, and records the interaction type.
+  ///
+  /// * This method is intended for internal use by the [WheelPicker] widget.
+  Future<void> _interactionShiftTo(
+    int index,
+    WheelPickerInteractionType interactionType,
+  ) async {
+    if (!_hasClients) return;
+    _setInteractionType(interactionType);
+    return await _shiftController(
+      this,
+      steps: _capIndex(index),
+      shiftAnimationStyle: _shiftAnimationStyle,
+      relativeToCurrent: false,
+    );
+  }
+
+  /// Shifts the wheel by a specified number of items, and records the interaction type.
+  ///
+  /// * This method is intended for internal use by the [WheelPicker] widget.
+  Future<void> _interactionShiftBy(
+    int steps,
+    WheelPickerInteractionType interactionType,
+  ) async {
+    if (!_hasClients) return;
+    _setInteractionType(interactionType);
+    return await _shiftController(
+      this,
+      steps: steps,
+      shiftAnimationStyle: _shiftAnimationStyle,
+      relativeToCurrent: true,
+    );
+  }
+
+  /// Shifts the wheel up by one item, and records the interaction type.
+  ///
+  /// * This method is intended for internal use by the [WheelPicker] widget.
+  Future<void> _interactionShiftUp(
+      WheelPickerInteractionType interactionType) async {
+    return await _interactionShiftBy(-1, interactionType);
+  }
+
+  /// Shifts the wheel down by one item, and records the interaction type.
+  ///
+  /// * This method is intended for internal use by the [WheelPicker] widget.
+  Future<void> _interactionShiftDown(
+      WheelPickerInteractionType interactionType) async {
+    return await _interactionShiftBy(1, interactionType);
+  }
+
+  /// Shifts the wheel up by one item.
+  Future<void> shiftUp() async {
+    return await _interactionShiftUp(WheelPickerInteractionType.control);
+  }
+
+  /// Shifts the wheel down by one item.
+  Future<void> shiftDown() async {
+    return await _interactionShiftDown(WheelPickerInteractionType.control);
+  }
+
+  /// Shifts the wheel by a specified number of items.
+  ///
+  /// Parameters:
+  /// - `steps`: The number of items to shift. A positive value shifts down, and a negative value shifts up.
+  Future<void> shiftBy(int steps) async {
+    return _interactionShiftBy(steps, WheelPickerInteractionType.control);
+  }
+
+  /// Shifts the wheel to the specified index.
+  ///
+  /// Parameters:
+  /// - `index`: The item number to jump to ranging from 0 to `itemCount` - 1.
+  Future<void> shiftTo(int index) async {
+    return _interactionShiftTo(index, WheelPickerInteractionType.control);
+  }
+
+  /// Sets the current item to the specified index.
+  ///
+  /// Parameters:
+  /// - `index`: The item number to jump to ranging from 0 to `itemCount` - 1
+  Future<void> setCurrent(int index) async {
+    if (!_hasClients) return;
+    _setInteractionType(WheelPickerInteractionType.control);
+    _jumpController(this, _capIndex(index));
+  }
+
+  /// The current selected item index.
+  ///
+  /// Returns:
+  /// - The current selected item index if the controller has clients (attached).
+  /// - `-1` if the controller has no clients (not attached).
+  int get selected => _hasClients ? _current : -1;
+
+  /// Disposes of the controller and its resources.
+  ///
+  /// Avoid using the controller after disposing it to prevent unexpected behavior.
+  void dispose() => _scrollController.dispose();
+}
+
+/// Shifts the controller's scroll position by a specified number of items with animation.
+///
+/// Parameters:
+/// - `controller`: The controller managing the scroll.
+/// - `steps`: The number of items to shift. A positive value shifts down, and a negative value shifts up.
+/// - `shiftAnimationStyle`: The animation style used for the shift.
+///
+/// This method ensures the controller has clients before performing the shift animation.
+///
+/// * Note that this is only supposed to be in the controller scope itself.
+Future<void> _shiftController(
+  WheelPickerController controller, {
+  required int steps,
+  required WheelShiftAnimationStyle shiftAnimationStyle,
+  required bool relativeToCurrent,
+}) async {
+  if (!controller._hasClients) return;
+  return await controller._scrollController.animateToItem(
+    (relativeToCurrent ? controller._scrollController.selectedItem : 0) + steps,
+    duration: shiftAnimationStyle.duration,
+    curve: shiftAnimationStyle.curve,
+  );
+}
+
+/// Jumps the controller's scroll position to the item at the given index.
+/// Parameters:
+/// - `controller`: The controller managing the scroll.
+/// - `index`: The item number to jump to ranging from 0 to `itemCount` - 1.
+///
+/// This method ensures the controller has clients before jumping.
+///
+/// * Note that this is only supposed to be in the controller scope itself.
+Future<void> _jumpController(
+  WheelPickerController controller,
+  int index,
+) async {
+  if (!controller._hasClients) return;
+  controller._scrollController.jumpToItem(index);
+}
