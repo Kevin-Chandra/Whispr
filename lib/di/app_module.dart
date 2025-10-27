5e1d304bfacc00f533d56b80b84a535a023078a2
Kevin Chandra <kevin.chandra@snappymob.com>
2025-10-27 15:22:40 +0800

    Add package info dependency and change onboarding and lock screen initial screen logic

--- lib/di/app_module.dart
+++ lib/di/app_module.dart
@@ -1,9 +1,13 @@
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:injectable/injectable.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 
 @module
 abstract class AppModule {
   @preResolve
   Future<FlutterSecureStorage> get secureStorage async =>
       const FlutterSecureStorage();
+
+  @preResolve
+  Future<PackageInfo> get packageInfo async => await PackageInfo.fromPlatform();
 }
